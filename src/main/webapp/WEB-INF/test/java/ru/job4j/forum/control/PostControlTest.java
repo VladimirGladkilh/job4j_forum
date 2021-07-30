c3e2daa9d798fd2b03e4d1c62a1d75ab3069918b
Vladimir Gladkilh <61043208+VladimirGladkilh@users.noreply.github.com>
2021-07-31 01:13:00 +0500

    1. Spring mock [#2096]

--- src/main/webapp/WEB-INF/test/java/ru/job4j/forum/control/PostControlTest.java
+++ src/main/webapp/WEB-INF/test/java/ru/job4j/forum/control/PostControlTest.java
@@ -1,23 +1,73 @@
 package ru.job4j.forum.control;
 
+import org.junit.Before;
 import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentCaptor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
 import ru.job4j.forum.Main;
+import ru.job4j.forum.model.Post;
+import ru.job4j.forum.repository.PostRepository;
 
+import java.util.Optional;
+
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
 
+
 @SpringBootTest(classes = Main.class)
 @AutoConfigureMockMvc
 class PostControlTest {
+
+    @MockBean
+    private PostRepository postRepository;
+
     @Autowired
     private MockMvc mockMvc;
+
+    @Before
+    public void setUp() {
+        Post post = new Post();
+        post.setName("Куплю ладу-грант. Дорого.");
+        when(postRepository.findById(anyLong()).orElse(null)).thenReturn(post);
+    }
+
+    @Test
+    @WithMockUser
+    public void saveRecord() throws Exception {
+        this.mockMvc.perform(post("/save")
+                .param("name", "Куплю ладу-грант. Дорого."))
+                .andDo(print())
+                .andExpect(status().is3xxRedirection());
+        ArgumentCaptor<Post> argument = ArgumentCaptor.forClass(Post.class);
+
+        verify(postRepository).save(argument.capture());
+        assertThat(argument.getValue().getName(), is("Куплю ладу-грант. Дорого."));
+    }
+
+    @Test
+    @WithMockUser
+    public void editRecord() throws Exception {
+        Post post = new Post();
+        post.setName("Куплю ладу-грант. Дорого.");
+        when(postRepository.findById(anyLong())).thenReturn(Optional.of(post));
+        this.mockMvc.perform(get("/edit")
+                .param("id", "1"))
+                .andDo(print())
+                .andExpect(status().is2xxSuccessful());
+    }
 
     @Test
     @WithMockUser
@@ -36,5 +86,6 @@ class PostControlTest {
                 .andExpect(status().isOk())
                 .andExpect(view().name("post/edit"));
     }
+
 
 }
