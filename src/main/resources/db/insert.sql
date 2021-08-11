f9ed8ca4079a0a06fa758243581740f4fe275d30
Vladimir Gladkilh <61043208+VladimirGladkilh@users.noreply.github.com>
2021-08-11 10:11:41 +0500

    merge in main

--- src/main/resources/db/insert.sql
+++ src/main/resources/db/insert.sql
@@ -3,3 +3,7 @@ values ('О чем этот форум?');
 insert into posts (name)
 values ('Правила форума.');
 
+insert into authorities (id, authority)
+values (1, 'ROLE_USER');
+insert into authorities (id, authority)
+values (2, 'ROLE_ADMIN');
